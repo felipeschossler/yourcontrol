c90e0bfc714b8eaece6baabddd02f34f89a3c736
Felipe Schossler <felipealvesschossler@gmail.com>
2018-10-10 12:14:56 -0300

    change head to include php and change the path of tmpl-styles

--- produto-consulta.php
+++ produto-consulta.php
@@ -10,25 +10,18 @@
     $grupo = selectTodosProdutos();
     //var_dump($grupo);
 ?>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <title>Consulta - Produto</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
-    <link rel="shortcut icon" href="img/favicon.ico" />
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <?php
+        include("tmpl/head.php");
+    ?>
 </head>
 
     <!--includes-->
-    <?php
-        include("header.html");
-        include("footer.html");
+    <?php 
+        include("tmpl/header.php");
+        include("tmpl/footer.php");
     ?>
 
     <!--table-->
