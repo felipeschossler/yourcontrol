447485b8a0042e3f2cbade8dce1f1081b1726e74
Felipe Schossler <felipealvesschosler@gmail.com>
2018-11-10 18:27:35 -0200

    add files of login page

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="./img/favicon.ico">
+        <title>Login - YourControl</title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <link href="./css/login.css" rel="stylesheet">
+    </head>
+
+    <body class="text-center">
+        <form class="form-signin">
+            <img class="mb-4" src="./img/y-image.png" alt="Your Control icon" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Por favor faça o login</h1>
+            <input type="text" id="codigoUsuario" class="form-control" placeholder="Código do usuário" required autofocus>
+            <input type="password" id="senhaUsuario" class="form-control" placeholder="Senha" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+            <p class="mt-5 mb-3 text-muted">YourControl&copy; - 2018</p>
+        </form>
+    </body>
+</html>
